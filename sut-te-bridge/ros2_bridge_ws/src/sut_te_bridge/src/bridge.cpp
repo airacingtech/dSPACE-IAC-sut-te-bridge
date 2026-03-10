975328b12ac092357678553ba53dbcb8b89d73b5
kevshin2002 <kevshin2002@gmail.com>
2026-03-09 14:08:36 -1000

    not aligned for one of them

--- sut-te-bridge/ros2_bridge_ws/src/sut_te_bridge/src/bridge.cpp
+++ sut-te-bridge/ros2_bridge_ws/src/sut_te_bridge/src/bridge.cpp
@@ -1466,7 +1466,7 @@ namespace bridge {
     else if (novatelID == 2)
     {
       currentNovatel = this->canBus->sim_interface_var.nova_tel_pwr_pak2_var;
-      frame_id = "gps_antenna_right";
+      frame_id = "gps_antenna_front";
       this->novaTelBestPosPublisher = this->novaTelBestPosPublisher2_;
       this->novaTelBestGNSSPosPublisher = this->novaTelBestGNSSPosPublisher2_;
       this->novaTelBestVelPublisher = this->novaTelBestVelPublisher2_;
