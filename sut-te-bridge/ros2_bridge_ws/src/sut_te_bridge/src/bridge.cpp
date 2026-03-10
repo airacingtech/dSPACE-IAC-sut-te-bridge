4f647a8e27e79702e36665334eb7be4f9ef3c125
kevshin2002 <kevshin2002@gmail.com>
2026-03-09 14:11:53 -1000

    front and right

--- sut-te-bridge/ros2_bridge_ws/src/sut_te_bridge/src/bridge.cpp
+++ sut-te-bridge/ros2_bridge_ws/src/sut_te_bridge/src/bridge.cpp
@@ -1453,7 +1453,7 @@ namespace bridge {
     if (novatelID == 1)
     {
       currentNovatel = this->canBus->sim_interface_var.nova_tel_pwr_pak1_var;
-      frame_id = "gps_antenna_left";
+      frame_id = "gps_antenna_front";
       this->novaTelBestPosPublisher = this->novaTelBestPosPublisher1_;
       this->novaTelBestGNSSPosPublisher = this->novaTelBestGNSSPosPublisher1_;
       this->novaTelBestVelPublisher = this->novaTelBestVelPublisher1_;
@@ -1466,7 +1466,7 @@ namespace bridge {
     else if (novatelID == 2)
     {
       currentNovatel = this->canBus->sim_interface_var.nova_tel_pwr_pak2_var;
-      frame_id = "gps_antenna_front";
+      frame_id = "gps_antenna_right";
       this->novaTelBestPosPublisher = this->novaTelBestPosPublisher2_;
       this->novaTelBestGNSSPosPublisher = this->novaTelBestGNSSPosPublisher2_;
       this->novaTelBestVelPublisher = this->novaTelBestVelPublisher2_;
@@ -1624,7 +1624,7 @@ namespace bridge {
     heading2.length = currentNovatel.heading_2_var.length;  
     heading2.heading = currentNovatel.heading_2_var.heading;
     heading2.pitch = currentNovatel.heading_2_var.pitch;
-    heading2.reserved = currentNovatel.heading_2_var.reserved;
+    heading2.reserved = currentNovatel.heading_2_var.reserved;/novatel_bottom/inspva
     heading2.heading_stdev = currentNovatel.heading_2_var.heading_stdev + noise(0.001);
     heading2.pitch_stdev = currentNovatel.heading_2_var.pitch_stdev;
     heading2.rover_stn_id[0] = currentNovatel.heading_2_var.rover_stn_id[0];
