77fb7c0105a521f9024c798355858ad96726fb1e
Daniel Warren <dpwarren455@gmail.com>
2026-03-23 11:01:47 -1000

    change tag of manual build to latest

--- build_dspace_bridge.sh
+++ build_dspace_bridge.sh
@@ -2,15 +2,15 @@
 
 # define general parameters
 TEAM="airacingtech"
-DATE=$(date '+%Y-%m-%d')
+TAG="latest"
 BASE_IMAGE="ghcr.io/airacingtech/art_ros_jazzy_cpu-built-dspace:stable"
 
 # define name of the image
-NAME_DEV=$TEAM/iac_dspace_bridge_dev:$DATE
-NAME_ASM_SOCKETCAN=$TEAM/iac_asm_socketcan_bridge:$DATE
-NAME_IAC_AURELION_ROS2=$TEAM/iac_aurelion_ros2_bridge:$DATE
-NAME_IAC_SOCKETCAN_DBW=$TEAM/iac_socketcan_dbw_bridge:$DATE
-NAME_FOXGLOVE=$TEAM/iac_ros2_foxglove_bridge:$DATE
+NAME_DEV=$TEAM/iac_dspace_bridge_dev:$TAG
+NAME_ASM_SOCKETCAN=$TEAM/iac_asm_socketcan_bridge:$TAG
+NAME_IAC_AURELION_ROS2=$TEAM/iac_aurelion_ros2_bridge:$TAG
+NAME_IAC_SOCKETCAN_DBW=$TEAM/iac_socketcan_dbw_bridge:$TAG
+NAME_FOXGLOVE=$TEAM/iac_ros2_foxglove_bridge:$TAG
 
 # build image
 echo "---------- build general dspace_bridge_dev ----------"
